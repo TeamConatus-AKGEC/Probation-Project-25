b1a3c979e7d76bec3b52c6e8179fad67c95feac2
Ritika katta <ritikakatta24@gmail.com>
2025-09-18 07:50:56 +0530

    Ritika katta Task2

--- Ritika_katta_Task2/guess_the_word.py
+++ Ritika_katta_Task2/guess_the_word.py
@@ -2,8 +2,20 @@ import random
 a= ["python", "machine", "learning", "data", "science"]
 b=random.choice(a)
 chances=6
-word=[]
-for i in range(len(a)):
-    word.append("_")
-Word=" ".join(word) 
-print("Word:",Word)
+list=[]
+for i in range(len(b)):
+    list.append("_")
+Word=" ".join(list) 
+while(chances>0 and "_"in Word):
+    print("Word:",Word)
+    c=input("enter the letter").lower()
+    if(c in b):
+        print("Correct!")
+        for i in range(len(b)):
+            if(b[i]==c):
+                list[i]=c
+                Word=" ".join(list)
+                print('Word:',Word)        
+    else:
+        chances=chances-1  
+        print("Wrong guess! Chances left:",chances)
